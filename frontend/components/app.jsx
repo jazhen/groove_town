bb093784cbbc39b88e2de282cd85ad7d9a369bbc
Jason Zhen <jasonzhen.mail@gmail.com>
2020-10-13 00:43:50 -0700

    Add css for AuthenicatedHeader

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Header from './header/header';
+import HeaderContainer from './header/header_container';
 import LoginFormContainer from './session_form/login_form_container';
 import SignupFormContainer from './session_form/signup_form_container';
 
 const App = () => {
   return (
     <>
-      <Header />
+      <HeaderContainer />
 
       <Switch>
         <Route path="/login" component={LoginFormContainer} />
--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -3,24 +3,32 @@ import { Link } from 'react-router-dom';
 
 const AuthenticatedHeader = ({ logout }) => {
   return (
-    <header>
-      <div className="nav-bar">
-        <Link className="nav-bar__index" to="/">
-          artistshack
-        </Link>
-        <div className="nav-bar__search-bar">Seach Bar</div>
-        <ul className="nav-bar__dropdown">
-          <button className="nav-bar__dropdown-button">Dropdown</button>
-          <li className="nav-bar__dropdown-list-item">
-            <button
-              className="nav-bar__logout"
-              type="submit"
-              onClick={logout()}>
-              logout
-            </button>
+    <header className="header">
+      <nav className="nav-bar">
+        <ul className="nav-bar__list-left">
+          <li className="nav-bar__list-left-item">
+            <Link className="nav-bar__index" to="/">
+              Artist Shack
+            </Link>
+          </li>
+          <li className="nav-bar__list-item">
+            <div className="nav-bar__search-bar">Search</div>
           </li>
         </ul>
-      </div>
+        <ul className="nav-bar__dropdown">
+          <button className="nav-bar__dropdown-button">Dropdown</button>
+          <ul className="nav-bar__dropdown-content">
+            <li className="nav-bar__dropdown-list-item">
+              <button
+                className="nav-bar__logout"
+                type="submit"
+                onClick={logout}>
+                logout
+              </button>
+            </li>
+          </ul>
+        </ul>
+      </nav>
     </header>
   );
 };
@@ -36,7 +44,7 @@ const UnauthenticatedHeader = () => {
             </Link>
           </li>
           <li className="nav-bar__list-item">
-            <div className="nav-bar__search-bar" />
+            <div className="nav-bar__search-bar">Search</div>
           </li>
         </ul>
         <ul className="nav-bar__list-right">
