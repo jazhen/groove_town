5148765eb659146d5a8f890898fafbaab3bf6eef
Jason Zhen <jasonzhen.mail@gmail.com>
2020-10-16 17:53:34 -0700

    Update auth nav bar to dropdown on click

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -9,7 +9,7 @@ const NavBarSiteList = () => {
           Artist Shack
         </Link>
       </li>
-      <li className="nav-bar__list-item">
+      <li className="nav-bar__site-list-item">
         <div className="nav-bar__search-bar">Search</div>
       </li>
     </ul>
@@ -22,24 +22,24 @@ const AuthenticatedHeader = ({ logout }) => {
       <nav className="nav-bar">
         <NavBarSiteList />
         <ul className="nav-bar__dropdown">
-          <li className="nav-bar__dropdown-list-item">
-            <span className="nav-bar__dropdown-span">Dropdown</span>
+          <button className="nav-bar__dropdown-button">Dropdown</button>
+          <li className="nav-bar__dropdown-menu">
+            <ul className="nav-bar__dropdown-content">
+              <li className="nav-bar__dropdown-content-list-item">
+                <Link className="nav-bar__dropdown-link" to="/">
+                  view collection
+                </Link>
+              </li>
+              <li className="nav-bar__dropdown-content-list-item">
+                <button
+                  className="nav-bar__logout"
+                  type="submit"
+                  onClick={logout}>
+                  logout
+                </button>
+              </li>
+            </ul>
           </li>
-          <ul className="nav-bar__dropdown-content">
-            <li className="nav-bar__dropdown-content-list-item">
-              <Link className="nav-bar__dropdown-link" to="/">
-                View collection
-              </Link>
-            </li>
-            <li className="nav-bar__dropdown-content-list-item">
-              <button
-                className="nav-bar__logout"
-                type="submit"
-                onClick={logout}>
-                logout
-              </button>
-            </li>
-          </ul>
         </ul>
       </nav>
     </header>
@@ -53,12 +53,12 @@ const UnauthenticatedHeader = () => {
         <NavBarSiteList />
         <ul className="nav-bar__user-list">
           <li className="nav-bar__user-list-item">
-            <Link className="nav-bar__link" to="/signup">
+            <Link className="nav-bar__session-link" to="/signup">
               signup
             </Link>
           </li>
           <li className="nav-bar__user-list-item">
-            <Link className="nav-bar__link" to="/login">
+            <Link className="nav-bar__session-link" to="/login">
               login
             </Link>
           </li>
