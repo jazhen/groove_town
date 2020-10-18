bb6a19dac3d47ff2b02500442ed5fcc822fbd437
Jason Zhen <jasonzhen.mail@gmail.com>
2020-10-17 21:09:07 -0700

    position session header text to left align

--- frontend/components/header/session_header.jsx
+++ frontend/components/header/session_header.jsx
@@ -5,7 +5,9 @@ const SessionHeader = () => {
   return (
     <header className="session-header ">
       <div className="session-header__container">
-        <Link to="/">Artist Shack</Link>
+        <Link className="session-header__link" to="/">
+          Artist Shack
+        </Link>
       </div>
     </header>
   );
