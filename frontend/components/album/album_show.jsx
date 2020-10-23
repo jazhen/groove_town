e2c7509fe4b9d5a74c7b79e470376565c3377b94
Jason Zhen <jasonzhen.mail@gmail.com>
2020-10-23 08:08:47 -0700

    add favicon

--- frontend/components/album/album_show.jsx
+++ frontend/components/album/album_show.jsx
@@ -4,8 +4,6 @@ const TabHeaders = ({ selectedTab, onTabChosen, tabs }) => {
   const active = selectedTab;
   const headers = tabs.map((tab, index) => {
     const { title } = tab;
-    // const selected = index === active ? 'album-show__tabs--active' : '';
-
     const selected = index === active ? 'active' : 'inactive';
 
     return (
@@ -18,35 +16,11 @@ const TabHeaders = ({ selectedTab, onTabChosen, tabs }) => {
           <span className="album-show__tab-title">{title}</span>
         </button>
       </li>
-
-      // return (
-      //   <li
-      //     className={`album-show__tabs-header-list-item ${selected}`}
-      //     key={index}>
-      //     <button
-      //       className="album-show__tabs-header-button"
-      //       onClick={() => onTabChosen(index)}>
-      //       <span className="album-show__tab-title">{title}</span>
-      //     </button>
-      //   </li>
     );
   });
 
   return <ul className="album-show__tabs-header-list">{headers}</ul>;
 };
-
-// const selected = index === active ? 'active' : 'inactive';
-
-// return (
-//   <li
-//     className={`album-show__tabs-header-list-item ${selected}`}
-//     key={index}>
-//     <button
-//       className={`album-show__tabs-header-button album-show__tabs-header-button--${selected}`}
-//       onClick={() => onTabChosen(index)}>
-//       <span className="album-show__tab-title">{title}</span>
-//     </button>
-//   </li>
 
 class AlbumShow extends React.Component {
   constructor(props) {
