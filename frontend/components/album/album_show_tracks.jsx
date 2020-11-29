b3edc0a9059fe878df65f8ff41fe47a86b51ce48
Jason Zhen <jasonzhen.mail@gmail.com>
2020-11-28 19:47:11 -0800

    change album tracks list to separate component

--- frontend/components/album/album_show_tracks.jsx
+++ frontend/components/album/album_show_tracks.jsx
@@ -1,5 +1,44 @@
 import React from 'react';
+
 // import AlbumShowTrack from './album_show_track';
+
+const AlbumShowTrackButton = ({
+  track,
+  player,
+  playing,
+  setPlaying,
+  currentTrack,
+  setCurrentTrack,
+  handlePlay,
+}) => {
+  const handleClick = () => {
+    setCurrentTrack(track);
+    setPlaying(true);
+    player.current.pause();
+    player.current.load();
+    player.current.play();
+  };
+
+  return currentTrack.ord === track.ord ? (
+    <button
+      type="button"
+      aria-label="play-audio"
+      className={
+        playing
+          ? 'fas fa-pause album-show__track-play-button'
+          : 'fas fa-play album-show__track-play-button'
+      }
+      onClick={handlePlay}
+    />
+  ) : (
+    <button
+      type="button"
+      aria-label="play-track"
+      className="fas fa-play album-show__track-play-button"
+      onClick={() => handleClick(track)}
+    />
+  );
+};
 
 const AlbumShowTracks = ({
   trackIds,
@@ -11,14 +50,6 @@ const AlbumShowTracks = ({
   setCurrentTrack,
   handlePlay,
 }) => {
-  const handleClick = (track) => {
-    setCurrentTrack(track);
-    setPlaying(true);
-    player.current.pause();
-    player.current.load();
-    player.current.play();
-  };
-
   return (
     <ul>
       {trackIds.map((trackId) => {
@@ -29,25 +60,15 @@ const AlbumShowTracks = ({
             {/* <AlbumShowTrack track={tracks[trackId]} /> */}
             <ul className="album-show__track">
               <li>
-                {currentTrack.ord === track.ord ? (
-                  <button
-                    type="button"
-                    aria-label="play-audio"
-                    className={
-                      playing
-                        ? 'fas fa-pause album-player__play-button'
-                        : 'fas fa-play album-player__play-button'
-                    }
-                    onClick={handlePlay}
-                  />
-                ) : (
-                  <button
-                    type="button"
-                    aria-label="play-track"
-                    className="fas fa-play album-player__play-button"
-                    onClick={() => handleClick(track)}
-                  />
-                )}
+                <AlbumShowTrackButton
+                  track={track}
+                  player={player}
+                  playing={playing}
+                  setPlaying={setPlaying}
+                  currentTrack={currentTrack}
+                  setCurrentTrack={setCurrentTrack}
+                  handlePlay={handlePlay}
+                />
               </li>
               <li className="album-show__track-ord">{`${track.ord}.`}</li>
               <li className="album-show__track-name">{track.name}</li>
