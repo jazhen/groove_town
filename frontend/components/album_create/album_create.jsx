2923cf48c1c7be6515823025de295172a0218135
Jason Zhen <jasonzhen.mail@gmail.com>
2020-12-01 00:49:03 -0800

    add styling to album create options

--- frontend/components/album_create/album_create.jsx
+++ frontend/components/album_create/album_create.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import AlbumForm from './album_form';
 
 const AlbumCreate = ({ user }) => {
@@ -30,47 +31,93 @@ const AlbumCreate = ({ user }) => {
     // contact: <Contact />,
   };
 
+  const handleTrackUpload = () => {};
+
   return (
     <div className="album-create">
-      <div className="album-create__tabs">
-        <button
-          type="button"
-          className="album-create__album-tab"
-          onClick={() => setSelectedTab('albumForm')}
-        >
-          {albumArtUrl ? (
-            <img
-              className="album-create__album-tab-art
+      <div className="album-create__tabs-container">
+        <div className="album-create__tabs">
+          <button
+            type="button"
+            className="album-create__album-tab"
+            onClick={() => setSelectedTab('albumForm')}
+          >
+            {albumArtUrl ? (
+              <img
+                className="album-create__album-tab-art
                 album-create__album-tab-art--file"
-              src={albumArtUrl}
-              alt="album art"
-            />
-          ) : (
-            <div
-              className="album-create__album-tab-art
+                src={albumArtUrl}
+                alt="album art"
+              />
+            ) : (
+              <div
+                className="album-create__album-tab-art
                 album-create__album-tab-art--empty"
-            />
-          )}
+              />
+            )}
 
-          <div className="album-create__album-tab-description">
-            <div className="album-create__album-tab-name">
-              {albumName || 'Untitled Album'}
-            </div>
-            <div className="album-create__album-tab-band">
-              by&nbsp;
-              <div className="album-create__album-tab-band-name">
-                {user.band}
+            <div className="album-create__album-tab-description">
+              <div className="album-create__album-tab-name">
+                {albumName || 'Untitled Album'}
+              </div>
+              <div className="album-create__album-tab-band">
+                by&nbsp;
+                <div className="album-create__album-tab-band-name">
+                  {user.band}
+                </div>
+              </div>
+              <div className="album-create__album-tab-date">
+                {formattedDate}
               </div>
             </div>
-            <div className="album-create__album-tab-date">{formattedDate}</div>
+          </button>
+          <button type="button" onClick={() => setSelectedTab('albumForm')}>
+            albumForm
+          </button>
+          <button type="button" onClick={() => setSelectedTab('albumForm')}>
+            albumForm
+          </button>
+        </div>
+        <div className="album-create__options">
+          <div className="album-create__options-tabs-title">TRACKS</div>
+
+          {/* <div className="album-create__options-add-track">
+            <div className="album-create__options-add-track-input">
+              add track
+            </div>
+            <div className="album-create__options-add-track-description">
+              291MB max per track, lossless .wav, .aif or .flac
+            </div>
+          </div> */}
+
+          <div className="album-create__options-add-track">
+            <input
+              type="file"
+              id="album-create__options-add-track-input"
+              className="album-create__options-add-track-input"
+              accept="audio/mpeg"
+              onChange={handleTrackUpload}
+            />
+            <label
+              htmlFor="album-create__options-add-track-input"
+              className="album-create__options-add-track-label"
+            >
+              add track
+            </label>
+            <div className="album-create__options-add-track-description">
+              291MB max per track, lossless .wav, .aif or .flac
+            </div>
           </div>
-        </button>
-        <button type="button" onClick={() => setSelectedTab('albumForm')}>
-          albumForm
-        </button>
-        <button type="button" onClick={() => setSelectedTab('albumForm')}>
-          albumForm
-        </button>
+
+          <div className="album-create__options-publish-container">
+            <button type="button" className="album-create__options-publish">
+              Publish
+            </button>
+            <Link to="/" className="album-create__options-cancel">
+              cancel
+            </Link>
+          </div>
+        </div>
       </div>
       <div className="album-create__active-tab">{tabs[selectedTab]}</div>
     </div>
