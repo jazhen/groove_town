5b7d3a810908a1df56115ac4f2dc03c891eb030a
Jason Zhen <jasonzhen.mail@gmail.com>
2020-12-05 07:06:27 -0800

    change loading icon to record

--- frontend/components/loading/loading.jsx
+++ frontend/components/loading/loading.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Loading = () => {
   return (
     <div className="fa-10x loading-container">
-      <i className="fas fa-spinner fa-spin loading-spinner" />
+      <i className="fas fa-compact-disc fa-spin loading-spinner" />
     </div>
   );
 };
